edba37c0ce1bc048251dc95341036a6be362b501
algofreak <algofreak@gajjars.com>
2014-04-29 17:28:37 -0700

    RabbitMQ - basic working example.

---
+++ M101J/src/main/java/com/rabbitmq/basics/Consumer.java
@@ -0,0 +1,61 @@
+package com.rabbitmq.basics;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * @author: pgajjar @date: 4/29/14 @time: 1:17 AM
+ */
+public class Consumer {
+    private static final Logger logger = Logger.getLogger(Consumer.class.getName());
+    private Connection connection = null;
+    private Channel channel = null;
+    private QueueingConsumer queueingConsumer = null;
+
+    public Consumer(String hostName) throws IOException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(hostName);
+        connection = connectionFactory.newConnection();
+    }
+
+    public void connect(String queueName) throws IOException {
+        channel = connection.createChannel();
+        channel.queueDeclare(queueName, false, false, true, null);
+        queueingConsumer = new QueueingConsumer(channel);
+        String consumerTag = channel.basicConsume(queueName, true, queueingConsumer);
+        logger.info("Received the consumerTag: " + consumerTag + " from server.");
+        logger.info(" [*] Waiting for messages. To exit press CTRL+C");
+    }
+
+    public void getMsgs() throws InterruptedException {
+        boolean shouldQuit = false;
+        while (!shouldQuit) {
+            QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            shouldQuit = Producer.shouldQuit(message);
+            logger.info("[x] Received '" + message + "'");
+        }
+    }
+
+    public void close() throws IOException {
+        channel.close();
+        connection.close();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length != 2) {
+            System.out.println("Usage: Consumer <hostname> <queuename>");
+            System.exit(-1);
+        }
+
+        Consumer consumer = new Consumer(args[0]);
+        consumer.connect(args[1]);
+        consumer.getMsgs();
+        consumer.close();
+    }
+}
---
+++ M101J/src/main/java/com/rabbitmq/basics/Producer.java
@@ -0,0 +1,51 @@
+package com.rabbitmq.basics;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * @author: pgajjar @date: 4/29/14 @time: 12:47 AM
+ */
+public class Producer {
+    private static final Logger logger = Logger.getLogger(Producer.class.getName());
+
+    private Connection connection = null;
+    private Channel channel = null;
+
+    public static boolean shouldQuit(String message) {
+        return message.equalsIgnoreCase("quit") || message.equalsIgnoreCase("exit") || message.equalsIgnoreCase("end");
+    }
+
+    public Producer(String hostName) throws IOException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(hostName);
+        connection = connectionFactory.newConnection();
+    }
+
+    public void sendMsg(String queueName, String message) throws IOException {
+        channel = connection.createChannel();
+        channel.queueDeclare(queueName, false, false, true, null);
+        channel.basicPublish("", queueName, null, message.getBytes());
+        logger.info(" [x] Sent '" + message + "'");
+    }
+
+    public void close() throws IOException {
+        channel.close();
+        connection.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Usage: Producer <hostname> <queuename> <message>");
+            System.exit(-1);
+        }
+
+        Producer producer = new Producer(args[0]);
+        producer.sendMsg(args[1], args[2]);
+        producer.close();
+    }
+}
