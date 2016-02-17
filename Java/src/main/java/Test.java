fb05b9bb3fed22ea133d3ddaed92c7c31a85890b
Pradip Gajjar <pgajjar@apple.com>
2016-02-16 23:40:58 -0800

    Gradle based new Java learning resources

---
+++ Java/src/main/java/Test.java
@@ -0,0 +1,15 @@
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+/**
+ * @author: pgajjar
+ * @since: 2/16/16
+ */
+public class Test {
+    public static void main(String[] args) {
+        Set<String> name = Sets.newHashSet();
+        name.add("Pradip");
+        name.add("Rakesh");
+    }
+}
